e00590e0642af14b096db118f630a62178dadf12
tiendvth <tiendvth2009015@fpt.edu.vn>
2022-07-30 14:26:31 +0700

    tien

---
+++ src/main/java/fpt/aptech/thieaddangvantien/ThiEadDangVanTienApplication.java
@@ -0,0 +1,13 @@
+package fpt.aptech.thieaddangvantien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiEadDangVanTienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThiEadDangVanTienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fpt/aptech/thieaddangvantien/ThiEadDangVanTienApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.aptech.thieaddangvantien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiEadDangVanTienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
